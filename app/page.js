bbba204f80e61b9b53a1f523d871665caf0bfed6
Vivaan-R21 <158853138+Vivaan-R21@users.noreply.github.com>
2025-03-19 20:13:36 -0700

    Added an external link to the resume tab in the navbar

--- app/page.js
+++ app/page.js
@@ -81,17 +81,23 @@ export default function Home() {
 
   useEffect(() => {
     const handleScroll = (e) => {
+      const href = e.target.getAttribute("href");
+      if (!href.startsWith("#")) {
+        return;
+      }
       e.preventDefault();
-      const targetId = e.target.getAttribute("href").slice(1);
+      const targetId = href.slice(1);
       const targetElement = document.getElementById(targetId);
-      const offset = 100; // Increase this value to ensure the section title is hidden behind the navbar
-      const elementPosition = targetElement.getBoundingClientRect().top;
-      const offsetPosition = elementPosition + window.pageYOffset - offset;
+      if (targetElement) {
+        const offset = 100; // Increase this value to ensure the section title is hidden behind the navbar
+        const elementPosition = targetElement.getBoundingClientRect().top;
+        const offsetPosition = elementPosition + window.pageYOffset - offset;
 
-      window.scrollTo({
-        top: offsetPosition,
-        behavior: "smooth",
-      });
+        window.scrollTo({
+          top: offsetPosition,
+          behavior: "smooth",
+        });
+      }
     };
 
     const links = document.querySelectorAll(".navbar a");
@@ -142,7 +148,9 @@ export default function Home() {
         <title>Vivaan Rajpurohit - Developer Portfolio</title>
       </Head>
       <nav className="navbar">
-        <a href="#" className="navbar-brand">Vivaan Rajpurohit</a>
+        <a href="#" className="navbar-brand">
+          Vivaan Rajpurohit
+        </a>
         <ul>
           <li>
             <a href="#skills">Skills</a>
@@ -154,7 +162,8 @@ export default function Home() {
             <a href="#awards">Awards</a>
           </li>
           <li>
-            <a href="#resume">Resume</a>
+            <a
+              href="https://bit.ly/VivaanRajpurohitsResume" target="_blank" rel="noopener noreferrer">Resume</a>
           </li>
           <li>
             <a href="#about">About me</a>
@@ -175,20 +184,20 @@ export default function Home() {
             <div className="text-content">
               <h1 id="MyName">Vivaan Rajpurohit</h1>
               <p className="about-me">
-                Experienced developer with 7 months of industrial work experience
-                at MyHomeWorkRewards, a Canadian platform that rewards students
-                for completing homework. Proficient in HTML,CSS, JavaScript,
-                PHP, and cloud services. Currently developing mobile and web
-                apps with Dart, Flutter, and Python to enhance user experiences,
-                with a focus on user retention and performance optimization.
-                Passionate about continuous learning and staying current with
-                emerging technologies. Completed FAA ground school and serve as
-                School Site Council Vice President, overseeing school funds.
-                Adept at problem-solving, critical thinking, and collaboration,
-                I leverage my expertise to deliver innovative solutions.
-                Achieving a 67% improvement in website performance and user
-                engagement, contributing to significant growth in user retention
-                and overall project success.
+                Experienced developer with 7 months of industrial work
+                experience at MyHomeWorkRewards, a Canadian platform that
+                rewards students for completing homework. Proficient in
+                HTML,CSS, JavaScript, PHP, and cloud services. Currently
+                developing mobile and web apps with Dart, Flutter, and Python to
+                enhance user experiences, with a focus on user retention and
+                performance optimization. Passionate about continuous learning
+                and staying current with emerging technologies. Completed FAA
+                ground school and serve as School Site Council Vice President,
+                overseeing school funds. Adept at problem-solving, critical
+                thinking, and collaboration, I leverage my expertise to deliver
+                innovative solutions. Achieving a 67% improvement in website
+                performance and user engagement, contributing to significant
+                growth in user retention and overall project success.
               </p>
             </div>
           </div>
@@ -198,7 +207,14 @@ export default function Home() {
           <hr />
           <div className="flip-card-grid">
             {skills.map((skill) => (
-              <div className="flip-card" key={skill.name}>
+              <div
+                className={`flip-card ${
+                  skill.name === "Dart" || skill.name === "JavaScript"
+                    ? "lowered-tile"
+                    : ""
+                }`}
+                key={skill.name}
+              >
                 <div className="flip-card-inner">
                   <div className="flip-card-front">
                     <img src={skill.image} alt={`${skill.name} Image`} />
@@ -223,28 +239,44 @@ export default function Home() {
           <div className="certificates">
             <div className="certificate-box">
               <div className="certificate-content">
-                <a href="https://www.freecodecamp.org/certification/Vivaan-Rajpurohit/responsive-web-design" target="_blank" rel="noopener noreferrer">
+                <a
+                  href="https://www.freecodecamp.org/certification/Vivaan-Rajpurohit/responsive-web-design"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   Responsive Web Design
                 </a>
               </div>
             </div>
             <div className="certificate-box">
               <div className="certificate-content">
-                <a href="https://www.freecodecamp.org/certification/Vivaan-Rajpurohit/javascript-algorithms-and-data-structures-v8" target="_blank" rel="noopener noreferrer">
+                <a
+                  href="https://www.freecodecamp.org/certification/Vivaan-Rajpurohit/javascript-algorithms-and-data-structures-v8"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   JavaScript Algorithms and Data Structures
                 </a>
               </div>
             </div>
             <div className="certificate-box">
               <div className="certificate-content">
-                <a href="https://udemy-certificate.s3.amazonaws.com/pdf/UC-495b0697-0509-4b9c-8bee-f42039e17a35.pdf" target="_blank" rel="noopener noreferrer">
+                <a
+                  href="https://udemy-certificate.s3.amazonaws.com/pdf/UC-495b0697-0509-4b9c-8bee-f42039e17a35.pdf"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   Python Certificate
                 </a>
               </div>
             </div>
             <div className="certificate-box">
               <div className="certificate-content">
-                <a href="https://udemy-certificate.s3.amazonaws.com/pdf/UC-7765a6bf-8866-4ac2-88de-13f0dd3e8720.pdf" target="_blank" rel="noopener noreferrer">
+                <a
+                  href="https://udemy-certificate.s3.amazonaws.com/pdf/UC-7765a6bf-8866-4ac2-88de-13f0dd3e8720.pdf"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   FAA Certificate
                 </a>
               </div>
@@ -296,7 +328,11 @@ export default function Home() {
             <div
               className="quote-wrapper"
               ref={quoteWrapperRef}
-              style={{ transform: `translateX(calc(-${currentQuoteIndex * 100}% + ${translateX}px))` }}
+              style={{
+                transform: `translateX(calc(-${
+                  currentQuoteIndex * 100
+                }% + ${translateX}px))`,
+              }}
             >
               {quotes.map((quote, index) => (
                 <div className="quote" key={index}>
@@ -312,10 +348,35 @@ export default function Home() {
           <h1 id="AbtMe">Contacts</h1>
           <hr />
           <ul className="contact-list">
-            <li>Email: <a href="mailto:vivaanrajpurohit11@gmail.com">vivaanrajpurohit11@gmail.com</a></li>
-            <li>Phone: <a href="tel:+12094863134">+1 (209) 486-3134</a></li>
-            <li>LinkedIn: <a href="https://www.linkedin.com/in/vivaan-rajpurohit-6930a1349/" target="_blank" rel="noopener noreferrer">Vivaan Rajpurohit</a></li>
-            <li>GitHub: <a href="https://github.com/Vivaan-R21" target="_blank" rel="noopener noreferrer">Vivaan-R21</a></li>
+            <li>
+              Email:{" "}
+              <a href="mailto:vivaanrajpurohit11@gmail.com">
+                vivaanrajpurohit11@gmail.com
+              </a>
+            </li>
+            <li>
+              Phone: <a href="tel:+12094863134">+1 (209) 486-3134</a>
+            </li>
+            <li>
+              LinkedIn:{" "}
+              <a
+                href="https://www.linkedin.com/in/vivaan-rajpurohit-6930a1349/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Vivaan Rajpurohit
+              </a>
+            </li>
+            <li>
+              GitHub:{" "}
+              <a
+                href="https://github.com/Vivaan-R21"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Vivaan-R21
+              </a>
+            </li>
           </ul>
         </div>
         <div className="section">
@@ -323,16 +384,29 @@ export default function Home() {
           <hr />
           <ul className="reference-list">
             <li>
-              <strong>Gabriel Aversano</strong><br />
-              Founder, MyHomeworkRewards<br />
-              Email: <a href="mailto:MyHomeWorkRewards@gmail.com">MyHomeWorkRewards@gmail.com</a><br />
+              <strong>Gabriel Aversano</strong>
+              <br />
+              Founder, MyHomeworkRewards
+              <br />
+              Email:{" "}
+              <a href="mailto:MyHomeWorkRewards@gmail.com">
+                MyHomeWorkRewards@gmail.com
+              </a>
+              <br />
               Phone: <a href="tel:+19059033272">+1-905-903-3272</a>
             </li>
             <li>
-              <strong>Monica Escamilla</strong><br />
-              Center Director, Digital NEST<br />
-              Email: <a href="mailto:mo.escamilla@digitalnest.org">mo.escamilla@digitalnest.org</a><br />
-              Phone: <a href="tel:+18317226378">+1-831-722-6378</a><br />
+              <strong>Monica Escamilla</strong>
+              <br />
+              Center Director, Digital NEST
+              <br />
+              Email:{" "}
+              <a href="mailto:mo.escamilla@digitalnest.org">
+                mo.escamilla@digitalnest.org
+              </a>
+              <br />
+              Phone: <a href="tel:+18317226378">+1-831-722-6378</a>
+              <br />
               Relationship: Mentor and Guide
             </li>
           </ul>
